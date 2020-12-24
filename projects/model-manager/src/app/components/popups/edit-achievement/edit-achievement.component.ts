bb7f8f7b4a3dc338feaf2e310bd645dcd9bd0e9d
Delphin Germa <delphin.germa@free.fr>
2020-12-24 05:19:27 +0100

    Achievements OK

--- projects/model-manager/src/app/components/popups/edit-achievement/edit-achievement.component.ts
+++ projects/model-manager/src/app/components/popups/edit-achievement/edit-achievement.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IAchievement } from 'game-engine';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-edit-achievement',
@@ -8,10 +10,26 @@ import { IAchievement } from 'game-engine';
 })
 export class EditAchievementComponent implements OnInit {
   public element: IAchievement;
+  public formGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    public bsModalRef: BsModalRef,
+    private formBuilder: FormBuilder,
+  ) {
+  }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      name: [this.element.name, Validators.required],
+    });
+  }
+
+  public save(): void {
+    if (this.formGroup.errors) {
+      return;
+    }
+    this.element.name = this.formGroup.controls.name.value;
+    this.bsModalRef.hide();
   }
 
 }
