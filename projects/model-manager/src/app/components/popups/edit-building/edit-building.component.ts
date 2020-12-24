489ce6e45edf6505356cf10689a3a4a7b1411a38
Delphin Germa <delphin.germa@free.fr>
2020-12-24 05:17:20 +0100

    Buildings ok

--- projects/model-manager/src/app/components/popups/edit-building/edit-building.component.ts
+++ projects/model-manager/src/app/components/popups/edit-building/edit-building.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { IBuilding } from 'game-engine';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IBuilding, IGameContext } from 'game-engine';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { ResourceTypeFormComponent } from '../../resource-type-form/resource-type-form.component';
 
 @Component({
   selector: 'app-edit-building',
@@ -8,10 +11,39 @@ import { IBuilding } from 'game-engine';
 })
 export class EditBuildingComponent implements OnInit {
   public element: IBuilding;
+  public context: IGameContext;
+  public formGroup: FormGroup;
+  public resources: string[];
 
-  constructor() { }
+  constructor(
+    public bsModalRef: BsModalRef,
+    private formBuilder: FormBuilder,
+  ) {
+  }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.resources = this.context.allResources.map(e => e.name);
+    this.formGroup = this.formBuilder.group({
+      name: [this.element.name, Validators.required],
+      cost: ResourceTypeFormComponent.create(this.formBuilder, this.element.cost),
+      produce: ResourceTypeFormComponent.create(this.formBuilder, this.element.produce),
+      consume: ResourceTypeFormComponent.create(this.formBuilder, this.element.consume),
+      storage: ResourceTypeFormComponent.create(this.formBuilder, this.element.storage),
+      maintenance: ResourceTypeFormComponent.create(this.formBuilder, this.element.maintenance),
+    });
+  }
+
+  public save(): void {
+    if (this.formGroup.errors) {
+      return;
+    }
+    this.element.name = this.formGroup.controls.name.value;
+    this.element.cost = ResourceTypeFormComponent.selected(this.formGroup.controls.cost as FormGroup);
+    this.element.produce = ResourceTypeFormComponent.selected(this.formGroup.controls.produce as FormGroup);
+    this.element.consume = ResourceTypeFormComponent.selected(this.formGroup.controls.consume as FormGroup);
+    this.element.storage = ResourceTypeFormComponent.selected(this.formGroup.controls.storage as FormGroup);
+    this.element.maintenance = ResourceTypeFormComponent.selected(this.formGroup.controls.maintenance as FormGroup);
+    this.bsModalRef.hide();
   }
 
 }
